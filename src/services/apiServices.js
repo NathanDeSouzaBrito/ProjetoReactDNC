6e74ad603ece5b2b7abe551dd2513a0c93a653b5
nathanSBrito <243589103+nathanSBrito@users.noreply.github.com>
2026-02-24 22:08:04 -0300

    feat(services): cria o metodo GET

---
+++ src/services/apiServices.js
@@ -0,0 +1,18 @@
+export const getApiGet = async (endpoint, params) => {
+    try {
+        const url = new URL(`https://dnc-react-api.vercel.app/${endpoint}`);
+        const response = await fetch(url, {
+            method: "GET"
+        })
+
+        if (!response.ok) {
+            console.error(`Error: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
